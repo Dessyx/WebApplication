3fe4213ecfb362ef4aa6c4744393d5ffc5cbf205
Deslynn Fenyes <ST10251981@vcconnect.edu.za>
2024-04-02 07:46:18 +0200

    Add project files.

---
+++ Models/UserTable.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Models
+{
+    public class UserTable : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
