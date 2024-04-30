4e9c5bad52755be405ed671e0fbee6396e84c1a4
Deslynn Fenyes <ST10251981@vcconnect.edu.za>
2024-04-30 15:11:17 +0200

    Admin
    The admin is able to insert product data into the database which is then displayed to the users.

---
+++ Controllers/ArtFormController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationLesson1.Models;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class ArtFormController : Controller
+    {
+        public ProductTable artwork = new ProductTable();
+
+
+
+        public ActionResult ArtForm(ProductTable p)  //connected to about page - sign up must be on another page not about 
+        {
+            var result = artwork.insert_product(p);
+            return RedirectToAction("Privacy", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult ArtForm()
+        {
+            return View(artwork);  //directs back to privacy page 
+        }
+
+
+    }
+}
---
+++ Controllers/MyWorkController.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+using System.Runtime.Intrinsics.Arm;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using WebApplicationLesson1.Models;
+
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class MyWorkController : Controller
+    {
+
+        Products product = new Products();
+
+        public IActionResult MyWork()
+        {
+            product.FetchData();
+            List<string> productNames = product.PopulateDropDown();
+            return View(product);
+        }       
+
+    }
+}
