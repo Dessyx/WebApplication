4beee5251acc7f4c4951569a35b22bd1839f9cd3
Deslynn Fenyes <ST10251981@vcconnect.edu.za>
2024-04-30 15:09:49 +0200

    Users
    Users are able to insert data into the database through the login and sign up pages.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationLesson1.Models;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class UserController : Controller
+    {
+        public UserTable uTable = new UserTable();
+
+        public ActionResult SignUp(UserTable user)  
+        {
+            var result = uTable.insert_User(user);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View(uTable); 
+        }
+
+
+    }
+}
